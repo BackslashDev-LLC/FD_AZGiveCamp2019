003ff57b6926f03d521aefab5339752a099090f8
John Ackerman <john@the-monastery.com>
2019-03-08 21:53:20 -0700

    starting move list

--- src/app/controls/checkbox/checkbox.component.ts
+++ src/app/controls/checkbox/checkbox.component.ts
@@ -1,53 +1,60 @@
-import { Component, Input, Output, EventEmitter, OnInit, forwardRef } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  forwardRef
+} from "@angular/core";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
 
 @Component({
-	selector: "bsd-checkbox",
-	templateUrl: "./checkbox.component.html",
-	styleUrls: ["./checkbox.component.scss"],
-	providers: [
-		{
-			provide: NG_VALUE_ACCESSOR,
-			useExisting: forwardRef(() => CheckboxComponent),
-			multi: true
-		}
-	]
+  selector: "bsd-checkbox",
+  templateUrl: "./checkbox.component.html",
+  styleUrls: ["./checkbox.component.scss"],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true
+    }
+  ]
 })
 export class CheckboxComponent implements OnInit, ControlValueAccessor {
-	checked: boolean = true;
+  checked: boolean = true;
 
-	@Input()
-	id: string;
-	@Input()
-	isSmallCheckbox: boolean = false;
-	@Input()
-	label: string;
-	@Input()
-	isDisabled: boolean = false;
+  @Input()
+  id: string;
+  @Input()
+  isSmallCheckbox: boolean = false;
+  @Input()
+  label: string;
+  @Input()
+  isDisabled: boolean = false;
 
-	@Output()
-	onItemChecked: EventEmitter<any> = new EventEmitter<any>();
+  @Output()
+  onItemChecked: EventEmitter<any> = new EventEmitter<any>();
 
-	constructor() {}
+  constructor() {}
 
-	ngOnInit() {}
+  ngOnInit() {}
 
-	onChange(checked: boolean) {
-		this.checked = checked;
-		this.propagateChange(this.checked);
+  onChange(checked: boolean) {
+    this.checked = checked;
+    this.propagateChange(this.checked);
 
-		this.onItemChecked.emit(this.checked);
-	}
+    this.onItemChecked.emit(this.checked);
+  }
 
-	writeValue(checked: boolean): void {
-		this.checked = checked;
-	}
+  writeValue(checked: boolean): void {
+    this.checked = checked;
+  }
 
-	propagateChange = (_: boolean) => {};
+  propagateChange = (_: boolean) => {};
 
-	registerOnChange(fn: any): void {
-		this.propagateChange = fn;
-	}
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
 
-	registerOnTouched(fn: any): void {}
+  registerOnTouched(fn: any): void {}
 }
--- src/app/movelist/movelist.component.ts
+++ src/app/movelist/movelist.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'fd-movelist',
-  templateUrl: './movelist.component.html',
-  styleUrls: ['./movelist.component.scss']
+  selector: "fd-movelist",
+  templateUrl: "./movelist.component.html",
+  styleUrls: ["./movelist.component.scss"]
 })
 export class MovelistComponent implements OnInit {
+  rooms = [
+    {
+      name: "Family Room",
+      items: ["Sofa", "Side Chair (Overstuffed)", "Cofee Table"]
+    },
+    {
+      name: "Kitchen",
+      items: ["Table", "Chairs (4)"]
+    },
+    {
+      name: "Room",
+      items: ["Queen Bed Frame", "Queen Mattress", "Night Stand w/Drawers"]
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
