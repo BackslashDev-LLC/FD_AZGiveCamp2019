580e3d54a0a6be9dcebdd7be673828a2298c6a19
John Ackerman <john@the-monastery.com>
2019-03-09 11:37:53 -0700

    Login Page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,18 @@ import { AuthService } from "../auth/auth.service";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  working: boolean = false;
+  username: string;
+  password: string;
+
   constructor(public authService: AuthService) {}
   ngOnInit() {}
+
+  signIn() {
+    this.working = true;
+
+    this.authService.login(this.username, this.password).then(() => {
+      this.working = false;
+    });
+  }
 }
