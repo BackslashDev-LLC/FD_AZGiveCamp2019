d40ec0056e6640b0ed2b529be9606e23aa8379cc
John Ackerman <john@the-monastery.com>
2019-03-09 13:45:39 -0700

    Fix date

--- src/app/models/fullWalkthrough.model.ts
+++ src/app/models/fullWalkthrough.model.ts
@@ -1,16 +1,33 @@
-import { FullRoom } from './fullRoom.model';
+import { FullRoom } from "./fullRoom.model";
 
 export class FullWalkthrough {
-    key: string;
-    dateTime: any;
-    rooms: FullRoom[] ;
-    public static getFirebase(fullWalk: FullWalkthrough){
-        var walk = {
-          key: fullWalk.key,
-          dateTime: fullWalk.dateTime,
-          rooms: FullRoom.getFirebase(fullWalk.rooms)
-        };
-    
-        return walk;
-      }
+  key: string;
+  dateTime: any;
+  rooms: FullRoom[];
+
+  fromFirebase(obj: any): FullWalkthrough {
+    const w = new FullWalkthrough();
+
+    w.key = obj.key;
+    w.rooms = obj.rooms;
+
+    const s = obj.dateTime.seconds;
+    const d = new Date(0);
+
+    d.setUTCSeconds(s);
+
+    w.dateTime = d;
+
+    return w;
   }
+
+  public static getFirebase(fullWalk: FullWalkthrough) {
+    var walk = {
+      key: fullWalk.key,
+      dateTime: fullWalk.dateTime,
+      rooms: FullRoom.getFirebase(fullWalk.rooms)
+    };
+
+    return walk;
+  }
+}
