c040a516eb1a00d12444f790b802316eb406f1f1
Dave Rael <dave@raelyard.com>
2019-03-09 09:33:56 -0700

    Add login component
    to support Firebase authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
+import { LoginComponent } from  './login/login.component';
 import { WalkthroughComponent } from "./walkthrough/walkthrough.component";
 import { MovelistComponent } from "./movelist/movelist.component";
 import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
+  { path: "login", component: LoginComponent },
   { path: "walkthrough", component: WalkthroughComponent },
   { path: "movelist", component: MovelistComponent },
   { path: "", component: HomeComponent }
