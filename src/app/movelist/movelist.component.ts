4f9e1f72b5f82f595be1ddf4641d607821012c3c
Dave Rael <dave@raelyard.com>
2019-03-09 16:18:29 -0700

    Fixed type of property exposed to template
    to fix the build with Typescript not liking the empty object without
    properties for the prod build

--- src/app/movelist/movelist.component.ts
+++ src/app/movelist/movelist.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class MovelistComponent implements OnInit {
   constructor(private savedWalkthroughService: SaveWalkthroughService, private activatedRoute: ActivatedRoute) {}
-  moveListInput = {};
+  public moveListInput: any = {};
 
   ngOnInit() {
     this.savedWalkthroughService
