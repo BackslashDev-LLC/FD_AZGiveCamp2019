7663ebed03f6fbe1f0550067f381d5992e3e0f59
thomas <thomas@localhost.localdomain>
2019-03-08 21:46:40 -0700

    Added firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import {WalkthroughService} from "../app/services/walkthrough.services";
 
 @Component({
   selector: "fd-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "furnishing-dignity";
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router, public walkthroughService: WalkthroughService) {}
 
   startWalkthrough() {
     this._router.navigate(["walkthrough"]);
@@ -17,5 +18,10 @@ export class AppComponent {
 
   goHome() {
     this._router.navigate([""]);
+  }
+  ngOnInit(){
+   this.walkthroughService.getWalkthroughById("Verry2019").subscribe(a => {
+     console.log(a);
+   })
   }
 }
