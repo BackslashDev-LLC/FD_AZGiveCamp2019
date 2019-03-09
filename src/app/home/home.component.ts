e3a78b3ad11179107c768e1147e98814c932a848
John Ackerman <john@the-monastery.com>
2019-03-09 12:09:40 -0700

    Actions

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { Client } from '../client';
+import { Component, OnInit } from "@angular/core";
+import { Client } from "../client";
 import { SelectionModel } from "@angular/cdk/collections";
 import { Router } from "@angular/router";
-import { WalkthroughService } from '../services/walkthrough.services';
+import { WalkthroughService } from "../services/walkthrough.services";
 
 @Component({
-  selector: 'fd-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "fd-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   searchTerm: string;
   searchResults: Client[];
   selectedClient: Client;
-  displayedColumns: string[] = ["name", "date"];
+  displayedColumns: string[] = ["name", "date", "actions"];
   selection: SelectionModel<Client>;
 
-  constructor(private _router: Router, public walkthroughService: WalkthroughService) { }
+  constructor(
+    private _router: Router,
+    public walkthroughService: WalkthroughService
+  ) {}
   result1: Client = {
     name: "AbleB",
     walkthroughDate: new Date("2/3/2019")
@@ -26,15 +29,17 @@ export class HomeComponent implements OnInit {
     walkthroughDate: new Date("3/10/2019")
   };
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   search() {
-   // this.walkthroughService.
+    // this.walkthroughService.
     this.searchResults = [this.result1, this.result2];
     const initialSelection = [];
     const allowMultiSelect = false;
-    this.selection = new SelectionModel<Client>(allowMultiSelect, initialSelection);
+    this.selection = new SelectionModel<Client>(
+      allowMultiSelect,
+      initialSelection
+    );
     //console.log(this.searchResults[0].name);
     //console.log(this.searchResults[1].name);
   }
@@ -50,15 +55,14 @@ export class HomeComponent implements OnInit {
     const numRows = this.searchResults.length;
     return numSelected == numRows;
   }
-  
+
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
-    this.isAllSelected() ?
-        this.selection.clear() :
-        this.searchResults.forEach(row => this.selection.select(row));
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.searchResults.forEach(row => this.selection.select(row));
   }
 
-  
   goToMoveList() {
     this._router.navigate(["movelist", this.selectedClient]);
   }
